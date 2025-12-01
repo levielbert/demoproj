2d60629d491238f34caba3597820b13050616c83
levielbert <levielbertg2@outlook.com>
2025-12-01 15:34:51 +0530

    created files

---
+++ infra/providers.tf
@@ -0,0 +1,15 @@
+
+terraform {
+  required_version = ">= 1.5"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+  backend "azurerm" {}  # configured at init time via -backend-config
+}
+
+provider "azurerm" {
+  features {}
+}
